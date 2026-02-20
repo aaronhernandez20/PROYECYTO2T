ce441a45c86bfad3b6f41d7d15c22bde11af8707
Aaron Hernandez <aahm152007@gmail.com>
2026-02-20 14:05:06 +0100

    creacion de clase personajes y main

---
+++ Proyecto2trimestre/src/proyecto2t/Main.java
@@ -0,0 +1,10 @@
+package proyecto2t;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Proyecto2trimestre/src/proyecto2t/Personajes.java
@@ -0,0 +1,5 @@
+package proyecto2t;
+
+public class Personajes {
+
+}
