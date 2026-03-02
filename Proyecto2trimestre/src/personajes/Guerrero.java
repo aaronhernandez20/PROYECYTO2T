1f269a79a3f73c3e5b2fa4a57fc8493116cdf7a2
Aaron Hernandez <aahm152007@gmail.com>
2026-03-02 09:40:14 +0100

    añado los constructores a las clases hijo de personaje(guerrero, mago y sacerdote)

--- Proyecto2trimestre/src/personajes/Guerrero.java
+++ Proyecto2trimestre/src/personajes/Guerrero.java
@@ -2,4 +2,9 @@ package personajes;
 
 public class Guerrero extends Personajes{
 
+	public Guerrero(String nombre, int vidaMax, int recursoMax, int ataqueBase, int defensaBase, int poderMagico) {
+		super(nombre, vidaMax, recursoMax, ataqueBase, defensaBase, poderMagico);
+		// TODO Auto-generated constructor stub
+	}
+
 }
