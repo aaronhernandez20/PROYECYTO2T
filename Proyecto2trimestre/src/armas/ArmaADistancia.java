a5b3bf898a33cacc1e2aa65ffaa5f6d5673cfb3c
Victor de Lucas <delucasfernandezvictor@gmail.com>
2026-03-05 09:09:40 +0100

    Armas casi terminadas

--- Proyecto2trimestre/src/armas/ArmaADistancia.java
+++ Proyecto2trimestre/src/armas/ArmaADistancia.java
@@ -1,5 +1,23 @@
 package armas;
 
-public class ArmaADistancia {
+import personajes.Personajes;
 
+public class ArmaADistancia extends Armas {
+
+    public ArmaADistancia(String nombre, int danoBase, double modificador, double probCritico,
+            double multiplicadorCritico) {
+        super(nombre, "A_DISTANCIA", danoBase, modificador, probCritico);
+    }
+
+    @Override
+    public int calcularDanio(Personajes atacante, Personajes defensor) {
+        int danoTotal = this.danoBase;
+
+        danoTotal = (int) (danoTotal * this.modificador);
+
+        if (Math.random() < this.probCritico) {
+        }
+
+        return danoTotal;
+    }
 }
