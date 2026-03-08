d5830a1860c6b4c915342f7b51db5c99cecd2889
Aaron Hernandez <aahm152007@gmail.com>
2026-03-08 18:59:30 +0100

    subclases mago y sacerdote acabadas

--- Proyecto2trimestre/src/personajes/Mago.java
+++ Proyecto2trimestre/src/personajes/Mago.java
@@ -2,11 +2,22 @@ package personajes;
 
 public class Mago extends Personajes {
 
-    public Mago(String nombre, int vidaMax, int recursoMax, int ataqueBase, int defensaBase, int poderMagico) {
+    private double multiplicadorMagico;
+
+    public Mago(String nombre, int vidaMax, int recursoMax, int ataqueBase,
+                int defensaBase, int poderMagico, double multiplicadorMagico) {
         super(nombre, vidaMax, recursoMax, ataqueBase, defensaBase, poderMagico);
-        // TODO Auto-generated constructor stub
+        this.multiplicadorMagico = multiplicadorMagico;
     }
 
+    // Calcula el poder real del hechizo multiplicando el poderMagico base
+    public int calcularPoderHechizo() {
+        return (int)(this.poderMagico * this.multiplicadorMagico);
+    }
 
-
+    // polimorfismo, el mago tiene su propia forma de actuar
+    @Override
+    public void realizarAccion(Personajes objetivo) {
+        System.out.println(this.nombre + " canaliza su magia y se prepara para actuar...");
+    }
 }
--- Proyecto2trimestre/src/personajes/Sacerdote.java
+++ Proyecto2trimestre/src/personajes/Sacerdote.java
@@ -1,10 +1,26 @@
 package personajes;
 
 public class Sacerdote extends Personajes {
+	  private int bonusSanacion;
 
-    public Sacerdote(String nombre, int vidaMax, int recursoMax, int ataqueBase, int defensaBase, int poderMagico) {
+    public Sacerdote(String nombre, int vidaMax, int recursoMax, int ataqueBase, int defensaBase, int poderMagico,int bonusSanacion) {
         super(nombre, vidaMax, recursoMax, ataqueBase, defensaBase, poderMagico);
         // TODO Auto-generated constructor stub
+        this.bonusSanacion = bonusSanacion;
+
+    }
+    @Override
+    public void curar(int cantidadVida) {
+    	//suma el bonus de sanacion
+        int curacionFinal = cantidadVida + this.bonusSanacion; 
+        //le pasa la vida final tras la curacion a la clase padre
+        super.curar(curacionFinal); 
+        System.out.println("(Bonus de sanación: +" + this.bonusSanacion + ")");
+    }
+    //polimorfsmo el sacerdote tiene su propia forma de actuar
+    @Override
+    public void realizarAccion(Personajes objetivo) {
+        System.out.println(this.nombre + " evalúa el estado del equipo y se prepara para actuar...");
     }
 
 }
