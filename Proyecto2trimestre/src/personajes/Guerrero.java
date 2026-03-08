d968492ab8481cfae7283a81df4ae6a18a728938
Aaron Hernandez <aahm152007@gmail.com>
2026-03-08 17:44:02 +0100

    subclase guerrero modificaciones

--- Proyecto2trimestre/src/personajes/Guerrero.java
+++ Proyecto2trimestre/src/personajes/Guerrero.java
@@ -9,19 +9,33 @@ public class Guerrero extends Personajes{
 		// TODO Auto-generated constructor stub
 		this.defensaAdrenalina= defensaAdrenalina;
 	}
+		//polimorfismo
+	@Override
+	public void realizarAccion(Personajes objetivo) {
+	    System.out.println(this.nombre + " se prepara para actuar...");
+	}
+	
+	private double probBloqueo;
+	
+
+	
 	@Override
 	public void recibirDano (int cantidadDano) {
 		//daño despues de aplicar la defensa por adrenalina
 		int danoReducido = cantidadDano - this.defensaAdrenalina;
 
+		// bloquea el ataque si el numero aleatorio es menor que el numero que se establezca de probabilidad al guerrero
+		if (Math.random() < this.probBloqueo) {
+		    System.out.println(this.nombre + " ¡ha bloqueado el ataque!");
+		    return; 
+	}
 		if (danoReducido < 0) {
 		    danoReducido = 0;
 		}
 		super.recibirDano(danoReducido);
+	
+	
 	}
-	//polimorfismo
-	@Override
-	public void realizarAccion(Personajes objetivo) {
-	    System.out.println(this.nombre + " se prepara para actuar...");
+
 	}
-}
+
