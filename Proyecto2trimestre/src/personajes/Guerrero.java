69dd7ecb7281b5bc6cc5b8ee6587e37365766ea2
Aaron Hernandez <aahm152007@gmail.com>
2026-03-08 20:42:10 +0100

    añado al constructor probBloqueo
    se crea del main los personajes y sus datos correspondientes, se crean
    las armas asignadas a cada personaje y el if que decide aleatoriamente
    que arma escoger

--- Proyecto2trimestre/src/personajes/Guerrero.java
+++ Proyecto2trimestre/src/personajes/Guerrero.java
@@ -6,7 +6,7 @@ public class Guerrero extends Personajes {
 
 	private int defensaAdrenalina;
 
-	public Guerrero(String nombre, int vidaMax, int recursoMax, int ataqueBase, int defensaBase, int poderMagico, int defensaAdrenalina) {
+	public Guerrero(String nombre, int vidaMax, int recursoMax, int ataqueBase, int defensaBase, int poderMagico, int defensaAdrenalina, double probBloqueo) {
 		super(nombre, vidaMax, recursoMax, ataqueBase, defensaBase, poderMagico);
 		// TODO Auto-generated constructor stub
 		this.defensaAdrenalina= defensaAdrenalina;
